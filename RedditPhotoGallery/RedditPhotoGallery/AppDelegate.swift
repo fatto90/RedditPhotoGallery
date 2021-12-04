c984b189f840b8f8773c4c0ad1dad2d8eb535b50
fattorim <fattorimatteo90@gmail.com>
2021-12-04 20:45:56 +0100

    Add RedditPhotoGallery project
    The xcode project has been created with iOS 13 as target.
    This project will be able to run on IPhone and IPad devices.
    The AppIcon have been added.

---
+++ RedditPhotoGallery/RedditPhotoGallery/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  RedditPhotoGallery
+//
+//  Created by Matteo Fattori on 04/12/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
