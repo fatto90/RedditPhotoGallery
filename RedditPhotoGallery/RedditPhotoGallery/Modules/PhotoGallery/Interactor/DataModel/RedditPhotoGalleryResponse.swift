82d64597834962d5248cfcb91441d9ea90dfba2e
fattorim <fattorimatteo90@gmail.com>
2021-12-05 11:27:13 +0100

    Add photo gallery fetch logic
    The data fetch logic has been added to PhotoGalleryInteractor.
    The data API response model has been added.
    In order to fetch data the interactor will dispatch a URLSession dataTask with URL composed by urlString and the input query.
    When fetch is over the result data is JSON parsed then returned through completionHandler.
    I decided to decouple result handling and API fetch logic in order to avoid further nesting.

---
+++ RedditPhotoGallery/RedditPhotoGallery/Modules/PhotoGallery/Interactor/DataModel/RedditPhotoGalleryResponse.swift
@@ -0,0 +1,28 @@
+//
+//  RedditPhotoGalleryResponse.swift
+//  RedditPhotoGallery
+//
+//  Created by Matteo Fattori on 05/12/21.
+//
+
+import Foundation
+
+struct RedditPhotoGalleryResponse: Codable {
+    let data: Data?
+    let message: String?
+    let error: Int?
+}
+
+struct Data: Codable {
+    let children: [ChildrenData]?
+}
+
+struct ChildrenData: Codable {
+    let data: ImageData?
+}
+
+struct ImageData: Codable {
+    let title: String?
+    let header_img: String?
+    let description_html: String?
+}
--- RedditPhotoGallery/RedditPhotoGallery/Modules/PhotoGallery/Interactor/PhotoGalleryInteractor.swift
+++ RedditPhotoGallery/RedditPhotoGallery/Modules/PhotoGallery/Interactor/PhotoGalleryInteractor.swift
@@ -9,4 +9,51 @@ import Foundation
 
 class PhotoGalleryInteractor {
     
+    private let session: URLSession
+    private let urlString: String
+    private let queryPlaceholder: String
+
+    init(session: URLSession = .shared) {
+        self.session = session
+        self.queryPlaceholder = "{KEYWORD}"
+        self.urlString = "https://www.reddit.com/r/\(self.queryPlaceholder)/top.json"
+    }
+    
+    // MARK: Public members
+    
+    public func getPhotoGallery(query: String, completionHandler:@escaping (_ data: [ChildrenData], _ error: Bool) -> ()) {
+        
+        if let url = URL(string: self.urlString.replacingOccurrences(of: self.queryPlaceholder, with: query)) {
+            let task = session.dataTask(with: url) {[weak self] data, response, error in
+                if let data = data {
+                    let jsonDecoder = JSONDecoder()
+                    do {
+                        let parsedJSON = try jsonDecoder.decode(RedditPhotoGalleryResponse.self, from: data)
+                        
+                        self?.handleGetPhotoGalleryResult(data: parsedJSON, completionHandler: completionHandler)
+                    } catch {
+                        self?.handleGetPhotoGalleryResult(data: nil, completionHandler: completionHandler)
+                    }
+                } else {
+                    self?.handleGetPhotoGalleryResult(data: nil, completionHandler: completionHandler)
+                }
+            }
+            task.resume()
+        } else {
+            self.handleGetPhotoGalleryResult(data: nil, completionHandler: completionHandler)
+        }
+    }
+    
+    // MARK: Private members
+    
+    private func handleGetPhotoGalleryResult(data: RedditPhotoGalleryResponse?, completionHandler: (_ data: [ChildrenData], _ error: Bool) -> ()) {
+        var error: Bool = true
+        var result: [ChildrenData] = []
+        if let childrens = data?.data?.children {
+            error = false
+            result = childrens
+        }
+        completionHandler(result, error)
+    }
+    
 }
